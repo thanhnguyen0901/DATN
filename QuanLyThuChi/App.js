0c3c95cb574b829ef6753b74ee0b5168f626f864
Law <vnphuong98@Laws-MacBook-Pro.local>
2019-06-11 11:06:21 +0700

    Update iOS
    Update iOS

---
+++ QuanLyThuChi/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Platform, Text, StyleSheet, View} from 'react-native';
+
+import ChiTieu from "./src/components/ChiTieu";
+import ThuNhap from "./src/components/ThuNhap";
+import ChuyenKhoan from "./src/components/ChuyenKhoan"
+import DieuChinhSoDu from "./src/components/DieuChinhSoDu"
+import ChonHangMuc from "./src/components/ChonHangMuc"
+
+export default class DATN extends Component{
+  render(){
+    return(
+      <ChiTieu />
+    );
+  }
+}
