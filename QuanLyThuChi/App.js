b90416cf284d4e2180d9e3a86d4886223a0a897f
vnphuong98 <33597902+vnphuong98@users.noreply.github.com>
2019-06-20 10:51:19 +0700

    Update App.js

--- QuanLyThuChi/App.js
+++ QuanLyThuChi/App.js
@@ -1,13 +1,13 @@
 // Import
-import React, {Component} from 'react';
-import { createAppContainer } from 'react-navigation';
-import AppNavigator from './AppNavigator';
+import React, { Component } from "react";
+import { createAppContainer } from "react-navigation";
+import AppNavigator from "./AppNavigator";
 
 // Const
 const AppContainer = createAppContainer(AppNavigator);
 
-export default class DATN extends Component{
-  render(){
-    return <AppContainer/>;
+export default class DATN extends Component {
+  render() {
+    return <AppContainer />;
   }
 }
